d47211dd7947cf4c8c0501eafa16cc4b99c61f59
MartinMSalas <ingmartinmsalas@gmail.com>
2025-01-27 10:48:33 -0300

    feat(api-config-server): set up public repo. Closes 1

---
+++ backend/api-config-server/src/test/java/com/mmstechnology/dmw/api_config_server/ApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmstechnology.dmw.api_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
