18711760cd27ef6f63336a0b2b50bb26b6bfd6a4
MartinMSalas <ingmartinmsalas@gmail.com>
2025-01-30 20:29:09 -0300

    fix(api-gateway-server): register solved. Close #13

--- backend/api-gateway-server/src/main/java/com/mmstechnology/dmw/api_gateway_server/ApiGatewayServerApplication.java
+++ backend/api-gateway-server/src/main/java/com/mmstechnology/dmw/api_gateway_server/ApiGatewayServerApplication.java
@@ -1,13 +1,13 @@
-package com.mmstechnology.dmw.api_gateway;
+package com.mmstechnology.dmw.api_gateway_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class ApiGatewayServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
 	}
 
 }
