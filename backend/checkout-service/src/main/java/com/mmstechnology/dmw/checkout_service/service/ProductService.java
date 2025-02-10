ef3e4b1476d099c46c8d848fca86a55f9de86e64
MartinMSalas <ingmartinmsalas@gmail.com>
2025-02-10 19:01:19 -0300

    feat(checkout-service): working circuitBreaker. Closes #22

--- backend/checkout-service/src/main/java/com/mmstechnology/dmw/checkout_service/service/ProductService.java
+++ backend/checkout-service/src/main/java/com/mmstechnology/dmw/checkout_service/service/ProductService.java
@@ -1,8 +1,9 @@
 package com.mmstechnology.dmw.checkout_service.service;
 
 import com.mmstechnology.dmw.checkout_service.model.dto.ProductDto;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ProductService {
 
-    ProductDto getProductById(String id);
+    ProductDto getProductById(@RequestParam String id);
 }
