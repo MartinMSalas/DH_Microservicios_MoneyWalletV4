d21fdd8c3420426333d50e5d0a1faaab5b853dd2
MartinMSalas <ingmartinmsalas@gmail.com>
2025-02-14 16:15:10 -0300

    feat(api-keycloak-server): add User Entity

---
+++ backend/api-keycloak-server/src/main/java/com/mmstechnology/dmw/api_keycloak_server/model/User.java
@@ -0,0 +1,19 @@
+package com.mmstechnology.dmw.api_keycloak_server.model;
+
+import lombok.Data;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.util.Set;
+
+@Data
+@EntityScan
+public class User {
+    String userId;
+    String username;
+    String email;
+    String firstName;
+    String lastName;
+    String password;
+    Set<String> roles;
+
+}
